0271fce9dff4d225e56dfefcf4d3893a00c34d73
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2026-04-27 14:57:22 -0400

    More config values

--- server/db/pool.js
+++ server/db/pool.js
@@ -9,14 +9,18 @@ const config = {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
-  // user: 'username',
-  // password: 'password',
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE
 };
 
 // ✍️ TODO 3: Create a separate config for production environments that use a connectionString
 
+const prodConfig = {
+  connectionString: process.env.PG_CONNECTION_STRING,
+}
+
 // ✍️ TODO 4: If PG_CONNECTION_STRING is available, use the prodConfig, otherwise use devConfig
 
-const pool = new Pool(config);
+const pool = new Pool(process.env.PG_CONNECTION_STRING ? prodConfig : devConfig);
 
 module.exports = pool;
